94a0c739985e962c016dd97d12e1468aa43cbfd7
vinochelo <vinochelo@github.com>
2026-03-23 13:17:36 -0500

    Fix: Simplificar configuracion vercel para api/index.js

---
+++ api/index.js
@@ -0,0 +1,3 @@
+const server = require('../dist/vercelHandler');
+
+module.exports = server.crearVercelServer();
--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { crearVercelServer } from '../src/vercelHandler';
-
-const app = crearVercelServer();
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
